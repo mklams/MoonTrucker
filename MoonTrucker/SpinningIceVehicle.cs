a0cbada552d8cf6b5de866d64e68ea1b669f3a7b
Tony Kolp <anthony.kolp@gmail.com>
2021-09-06 16:45:44 -0500

    Kinda better ice car

--- MoonTrucker/SpinningIceVehicle.cs
+++ MoonTrucker/SpinningIceVehicle.cs
@@ -6,13 +6,17 @@ using Microsoft.Xna.Framework.Input;
 
 namespace MoonTrucker
 {
-    public class VehicleWithMomentum
+    public class SpinningIceVehicle
     {
         private readonly float _screenWidth;
         private readonly float _screenHeight;
-        private Vector2 _velocity;
-        private float _angle => MathF.Atan(_velocity.Y/_velocity.X);
-        private float _lastAngle;
+        private Vector2 _velocity => _speed * _direction;
+        private float _speed;
+        private Vector2 _direction => new Vector2(MathF.Cos(MathHelper.ToRadians(_angle)), MathF.Sin(MathHelper.ToRadians(_angle)));
+        /*
+         * Angle in degrees
+         */
+        private float _angle;
         private Vector2 _position;
         private Sprite _sprite { get; set; }
         private bool _forward;
@@ -21,10 +25,10 @@ namespace MoonTrucker
         private const float MIN_SPEED = .5f;
         private const float DEGREES_TO_ROTATE = 2f;
 
-        public VehicleWithMomentum(Sprite sprite, Vector2 pos, float screenWidth, float screenHeight)
+        public SpinningIceVehicle(Sprite sprite, Vector2 pos, float screenWidth, float screenHeight)
         {
             _position = pos;
-            _velocity = new Vector2(0.001f,0f);
+            _speed = 0f;
             _forward = true;
             _stopped = true;
             _sprite = sprite;
@@ -34,16 +38,11 @@ namespace MoonTrucker
 
         public void Draw()
         {
-            var angle = (_stopped)? _lastAngle : _angle;
-            _sprite.Draw(_position, angle);
+            _sprite.Draw(_position, _angle);
         }
 
         public void UpdateVehicle(KeyboardState keyboardState, GameTime gameTime)
         {
-            if(!_stopped)
-            {
-                _lastAngle = _angle;
-            }
             if (keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.W))
             {
                 handleUpKey();
@@ -63,11 +62,11 @@ namespace MoonTrucker
                 handleRightKey();
             }
             _position += _velocity;
-            _velocity *= .99f;
+            _speed *= .99f; //friction
             var clampedPos = Vector2.Clamp(_position, new Vector2(0, 0), new Vector2(_screenWidth, _screenHeight));
             if(clampedPos != _position){
                 _stopped = true;
-                _velocity *= .001f;
+                _speed = 0f;
                 _position = clampedPos;
             } 
         }
@@ -78,9 +77,7 @@ namespace MoonTrucker
             {
                 _stopped = false;
                 _forward = true;
-                var newVel = new Vector2((float)Math.Cos(_lastAngle), (float)Math.Sin(_lastAngle));
-                newVel.Normalize();
-                _velocity = newVel;
+                _speed = 1f;
             }
             else if(_forward)
             {
@@ -98,10 +95,7 @@ namespace MoonTrucker
             {
                 _stopped = false;
                 _forward = false;
-                var newVel = new Vector2(MathF.Cos(_lastAngle), MathF.Sin(_lastAngle));
-                newVel.Normalize();
-                newVel = newVel * -1f;
-                _velocity = newVel;
+                _speed = -1f;
             }
             else if(!_forward){
                 accelerate();
@@ -118,11 +112,7 @@ namespace MoonTrucker
                 return;
             }
             var degreesToRotate = (_forward)? -DEGREES_TO_ROTATE : DEGREES_TO_ROTATE;
-            _velocity = this.rotate(_velocity, degreesToRotate);
-            if(!_forward)
-            {
-                _velocity = -_velocity;
-            }
+            _angle += degreesToRotate;
         }
 
         private void handleRightKey()
@@ -132,37 +122,22 @@ namespace MoonTrucker
                 return;
             }
             var degreesToRotate = (_forward)? DEGREES_TO_ROTATE : -DEGREES_TO_ROTATE;
-            _velocity = this.rotate(_velocity, degreesToRotate);
-            if(!_forward)
-            {
-                _velocity = -_velocity;
-            }
-        }
-
-        private Vector2 rotate(Vector2 vector, float degrees) 
-        {
-            float Vx, Vy;
-            Vx = vector.Length()*MathF.Cos(_angle + MathHelper.ToRadians(degrees));
-            Vy = vector.Length()*MathF.Sin(_angle + MathHelper.ToRadians(degrees));
-            return new Vector2(Vx, Vy);
+            _angle += degreesToRotate;
         }
 
         private void accelerate()
         {
-            var newVel = _velocity *= 1.05f;
-            if(newVel.Length() < MAX_SPEED){
-                _velocity = newVel;
+            _speed += 0.5f;
+            if(_speed > MAX_SPEED){
+                _speed = MAX_SPEED;
             }
         }
 
         private void decelerate()
         {
-            var newVel = _velocity *= 0.9f;
-            if(newVel.Length() > MIN_SPEED){
-                _velocity = newVel;
-            }
-            else
-            {
+            _speed*=.8f;
+            if(_speed < MIN_SPEED){
+                _speed = 0f;
                 _stopped = true;
             }
         }
