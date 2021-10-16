8d34f3f8658dc25ccde5d8aed8f517c4aae05f3c
mark <mklams@gmail.com>
2021-10-16 14:26:30 -0500

    Add walls

---
+++ MoonTrucker/Core/GameSave.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+
+namespace MoonTrucker.Core
+{
+
+    [Serializable]
+    public struct SaveGame
+    {
+        List<Score> HighScores;
+    }
+
+    public class Score
+    {
+        int HitTotal;
+        string Name;
+    }
+
+    public class GameSave
+    {
+        public GameSave()
+        {
+        }
+
+        public void SaveToDevice()
+        {
+            
+        }
+    }
+}
